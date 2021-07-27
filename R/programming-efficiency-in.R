9f8f971f0d8d3c2d2505cf323443c1e7acc461bb
Sebastian Marin <marin343@umn.edu>
2021-07-26 19:46:44 -0500

    update ignored files

---
+++ R/programming-efficiency-in.R
@@ -0,0 +1,14 @@
+# purpose: to understand ways to efficiently program in R
+# author: sebastian marin
+
+# set working directory to where the current file is located
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# update current packages (this could take awhile)
+update.packages()
+
+# load libraries
+install.packages('pacman')
+pacman::p_load( tidyverse, psych, anMC, MASS, rbenchmark )
+
+# let's start coding! ????
