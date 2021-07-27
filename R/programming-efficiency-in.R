a2ddb1b66a5b1585345758b47f9f80a7b6b310df
Sebastian Marin <marin343@umn.edu>
2021-07-27 12:32:54 -0500

    final update

--- R/programming-efficiency-in.R
+++ R/programming-efficiency-in.R
@@ -11,37 +11,30 @@ update.packages()
 install.packages('pacman')
 pacman::p_load( tidyverse, psych, anMC, MASS, rbenchmark )
 
-# let's start coding! ????
+# let's start coding! 
 
+get_random_df <- 
+  function(n_elements, ncol, min = 0, max = 1, seed = 42) {
+    seed <- set.seed(seed)
+    n <- n_elements #number of elements in a vector
+    x <- runif(n, min = min, max = max) #random vector of real numbers
+    x <- matrix(x, ncol=ncol)
+    x <- as.data.frame(x)
+    return(x)
+  }
 
-## Factor analysis 
-# ```{r}
-# 
-# ```
-# 
-# ```{r}
-# cfa <- 
-#   foreach (i = qref, .multicombine = T, .errorhandling = "pass") %dopar% {
-#     cfa(
-#       model =  "skill =~ tc0 + tc1 + tc2 + tc3 + tc4 + tc5 + tc6 + tc7 + tc8 + tc9", 
-#       data = ValData[ValData$qref == i,], 
-#       ordered = c('tc0','tc1','tc2','tc3','tc4','tc5','tc6','tc7','tc8','tc9')
-#     )
-#   }
-# ```
-# 
-# ```{r, model values}
-# models = foreach(i = 1:length(valCFA), .multicombine = T, .errorhandling = "pass") %dopar% {
-#   nobs <- inspect(valCFA[[i]], what ='nobs')
-#   fm <- fitMeasures(valCFA[[i]], c("chisq.scaled","df.scaled","pvalue.scaled","cfi.scaled","rmsea.scaled","srmr"))
-#   maxLoading <- max(lavInspect(valCFA[[i]], what='est')$lambda)
-#   minLoading <- min(lavInspect(valCFA[[i]], what='est')$lambda)
-#   
-#   as.list(c(qref[i], nobs, fm, maxLoading, minLoading))
-# }
-# ```
-#
-# ```{r, model output}
-# models_output = rbindlist(models, use.names = F)
-# ```
+n <- 1e5
+col <- 100
+data <- get_random_df(n_elements = n, ncol = col)
 
+rbenchmark::benchmark(
+  rds = saveRDS(data, "../data/data.rds"),
+  utils = write.csv(data, "../data/data_utils.csv"),
+  readr = write_csv(data, "../data/data_readr.csv"),
+  data.table = fwrite(data,"../data/data_dt.csv"), 
+  vroom = vroom_write(data, "../data/data_vroom.csv"),
+  
+  replications = 1,
+  columns = c('test', 'elapsed'),
+  order = 'elapsed'
+)
